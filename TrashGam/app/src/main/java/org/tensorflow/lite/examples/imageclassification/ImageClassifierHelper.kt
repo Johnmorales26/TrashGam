cd1a45a5263b0fb6901c750d5d459bd213821dc5
JohnMorales26 <johnta2610@hotmail.com>
2022-11-27 15:30:38 -0600

    Restore others models of IA

--- TrashGam/app/src/main/java/org/tensorflow/lite/examples/imageclassification/ImageClassifierHelper.kt
+++ TrashGam/app/src/main/java/org/tensorflow/lite/examples/imageclassification/ImageClassifierHelper.kt
@@ -75,6 +75,10 @@ class ImageClassifierHelper(
 
         val modelName =
             when (currentModel) {
+                MODEL_MOBILENETV1 -> "mobilenetv1.tflite"
+                MODEL_EFFICIENTNETV0 -> "efficientnet-lite0.tflite"
+                MODEL_EFFICIENTNETV1 -> "efficientnet-lite1.tflite"
+                MODEL_EFFICIENTNETV2 -> "efficientnet-lite2.tflite"
                 MODEL_WASTE_SORTING -> "wastesorting_model.tflite"
                 else -> "wastesorting_model.tflite"
             }
@@ -148,7 +152,11 @@ class ImageClassifierHelper(
         const val DELEGATE_CPU = 0
         const val DELEGATE_GPU = 1
         const val DELEGATE_NNAPI = 2
-        const val MODEL_WASTE_SORTING = 1
+        const val MODEL_MOBILENETV1 = 0
+        const val MODEL_EFFICIENTNETV0 = 1
+        const val MODEL_EFFICIENTNETV1 = 2
+        const val MODEL_EFFICIENTNETV2 = 3
+        const val MODEL_WASTE_SORTING = 4
 
         private const val TAG = "ImageClassifierHelper"
     }
